afd3e1c54912a553a7225c29b147436a6315be14
ianswarbrick <swarbrick@gmail.com>
2021-12-30 09:05:44 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,62 @@
+# Risk Main.py
+
+from torch import *
+
+
+# Create grid
+
+# define valid cells with continent.
+# 0 - not used
+# 1 - North america
+# 2 - south america
+# 3 - Europe
+# 4 - Africa
+# 5 - Asia
+# 6 - Australia
+
+# create grid with player owners.
+
+# define allowed connections.
+# use 8 directions, 0-7
+# 0 = up
+# 1 = NE
+# 2 = E
+# ..etc
+# 7 = NW
+
+# define players
+
+# randomly assign cells to players.
+
+# foreach player
+# randomly add soldiers to cells.
+
+
+#
+# Main loop
+#
+
+# take turns per player
+# figure out points to start.
+# points per territory
+# max(3, (cells_owned/3))
+# add for continents
+# cash in cards ? force if 5 cards....
+
+# let player assign troops to territories
+
+# Attacks
+
+# dice roll etc, blitz only.
+
+# Check if winner ( > 70% of territory)
+
+# end attack? or repeat.
+
+# Give card if earned
+
+# move troops?
+
+
+
+
